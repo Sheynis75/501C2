89e5b3d611b899bd33edb223d073e863a5579d28
Jahyung Yun <jahyun@bu.edu>
2021-12-04 15:50:50 -0500

    Added header authentication to flask

--- Flask Server/501Flask.py
+++ Flask Server/501Flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,6 +7,13 @@ app = Flask(__name__)
 ##    return send_file( 'README.md', as_attachment = True)
 @app.route('/')
 def test():
-    return 'Test'
+    headers = request.headers
+    auth = headers.get("ch0nk")
+    if auth == 'lol':
+        
+        #return jsonify({"message": "OK: Authorized"}), 200
+        return "Yay"
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
